2e1a76a6bdeed95ecf53a39ee2c5e81a5d64a5e8
twkfung <tonyfung1205@hotmail.com>
2023-01-11 08:28:31 +0000

    Deploying to gh-pages from @ twkfung/ycpspta-www@e93252e83d0cc03b9fc1b06adf36f99b2856ad8d 🚀

---
+++ _next/static/RTqMx698PzeWyCRy6yNnE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/cf14b2e59dfdf3bd.css","static/chunks/pages/index-18061358a6efdce8.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
