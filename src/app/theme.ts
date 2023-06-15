e1ef758b8ef4b62343213b7eb226e04ad3e4f947
Tony FUNG <twkfung@ust.hk>
2023-06-15 16:15:14 +0800

    feat: use responsive font sizes in theme

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,21 +1,23 @@
-import { createTheme } from "@mui/material/styles"
+import { createTheme, responsiveFontSizes } from "@mui/material/styles"
 import { lightBlue, orange } from "@mui/material/colors"
 import { fontsSans, fontsMono } from "./fonts"
 
-export const theme = createTheme({
-  palette: {
-    primary: {
-      main: lightBlue[600],
+export const theme = responsiveFontSizes(
+  createTheme({
+    palette: {
+      primary: {
+        main: lightBlue[600],
+      },
+      secondary: {
+        main: orange[600],
+      },
     },
-    secondary: {
-      main: orange[600],
+    typography: {
+      allVariants: {
+        fontFamily: [...fontsSans, ...fontsMono]
+          .map((font) => font.style.fontFamily)
+          .join(","),
+      },
     },
-  },
-  typography: {
-    allVariants: {
-      fontFamily: [...fontsSans, ...fontsMono]
-        .map((font) => font.style.fontFamily)
-        .join(","),
-    },
-  },
-})
+  })
+)
