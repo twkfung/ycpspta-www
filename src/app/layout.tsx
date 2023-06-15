53c7f7c79ba3a2416f11ff329bc730ccd7915f42
Tony FUNG <twkfung@ust.hk>
2023-06-15 16:12:46 +0800

    feat: specify theme colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="zh">
       <body style={{ fontFamily: fontFamily }}>
         <Providers>{children}</Providers>
       </body>
--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,7 +1,16 @@
 import { createTheme } from "@mui/material/styles"
+import { lightBlue, orange } from "@mui/material/colors"
 import { fontsSans, fontsMono } from "./fonts"
 
 export const theme = createTheme({
+  palette: {
+    primary: {
+      main: lightBlue[600],
+    },
+    secondary: {
+      main: orange[600],
+    },
+  },
   typography: {
     allVariants: {
       fontFamily: [...fontsSans, ...fontsMono]
