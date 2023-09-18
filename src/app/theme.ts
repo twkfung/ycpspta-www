ea407c13dabb1f27b5eb88aba80b8e2d3c6754ac
Tony FUNG <twkfung@ust.hk>
2023-09-18 16:42:40 +0800

    refactor: convert font family creation into a function

--- src/app/theme.ts
+++ src/app/theme.ts
@@ -1,6 +1,16 @@
 import { createTheme, responsiveFontSizes } from "@mui/material/styles"
 import { lightBlue, orange } from "@mui/material/colors"
 import { fontsSans, fontsMono } from "./fonts"
+import { NextFont } from "next/dist/compiled/@next/font"
+
+function createFontFamily(
+  fontsSans: NextFont[],
+  fontsMono: NextFont[],
+): string {
+  return [...fontsSans, ...fontsMono]
+    .map((font) => font.style.fontFamily)
+    .join(",")
+}
 
 export const theme = responsiveFontSizes(
   createTheme({
@@ -14,10 +24,8 @@ export const theme = responsiveFontSizes(
     },
     typography: {
       allVariants: {
-        fontFamily: [...fontsSans, ...fontsMono]
-          .map((font) => font.style.fontFamily)
-          .join(","),
+        fontFamily: createFontFamily(fontsSans, fontsMono),
       },
     },
-  })
+  }),
 )
