076125a2c132b77529fccfbcb0152c3e625da512
Tony FUNG <twkfung@ust.hk>
2023-09-20 18:09:15 +0800

    feat: handle fetching with reload

--- src/lib/shared/components/Posts.tsx
+++ src/lib/shared/components/Posts.tsx
@@ -2,14 +2,17 @@
 
 import { logger } from "@/lib/pino"
 import { type WpPost, wpClient } from "@/lib/wpapi"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useCallback } from "react"
 import {
   Paper,
   Typography,
   Stack,
   Divider,
   CircularProgress,
+  Box,
+  Button,
 } from "@mui/material"
+import { styled } from "@mui/material/styles"
 import { Markdown } from "@/lib/shared/components"
 import { WpEnv } from "@/lib/wpapi/WpEnv"
 
@@ -18,31 +21,73 @@ type Props = {
   tagSlug: WpEnv.wpTagSlugs
 }
 
+const CenteredBox = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100vh",
+  maxHeight: "400px",
+})
+
 export function Posts({ categorySlug, tagSlug }: Props) {
   const [error, setError] = useState<unknown>(null)
   const [loading, setLoading] = useState(true)
   const [posts, setPosts] = useState<WpPost[]>([])
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const wpPosts = await wpClient.loadPublishedPosts({
-          categorySlug,
-          tagSlug,
-        })
-        setPosts(wpPosts)
-      } catch (err) {
-        logger.error(err, "error fetching posts")
-        setError(err)
-      } finally {
-        setLoading(false)
-      }
+  const fetchData = useCallback(async () => {
+    try {
+      const wpPosts = await wpClient.loadPublishedPosts({
+        categorySlug,
+        tagSlug,
+      })
+      setPosts(wpPosts)
+    } catch (err) {
+      logger.error(err, "error fetching posts")
+      setError(err)
+      setPosts([])
+    } finally {
+      setLoading(false)
     }
-    fetchData()
   }, [categorySlug, tagSlug])
+
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
+
+  const handleReload = async () => {
+    setError(null)
+    setLoading(true)
+    setPosts([])
+    await fetchData()
+  }
+
+  if (error) {
+    return (
+      <section>
+        <CenteredBox>
+          <Typography variant="h5" gutterBottom color="error">
+            Error loading data
+          </Typography>
+          <Button variant="contained" onClick={handleReload}>
+            Reload
+          </Button>
+        </CenteredBox>
+      </section>
+    )
+  }
+
+  if (loading) {
+    return (
+      <section>
+        <CenteredBox>
+          <CircularProgress />
+        </CenteredBox>
+      </section>
+    )
+  }
 
   return (
     <section>
-      {loading && <CircularProgress />}
       <Stack divider={<Divider flexItem />}>
         {posts.map((post: WpPost) => (
           <Paper
