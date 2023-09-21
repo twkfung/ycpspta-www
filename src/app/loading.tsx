a215d3b57303021aa9a6bd2d5adb51c4dfb448ee
Tony FUNG <twkfung@ust.hk>
2023-09-21 13:30:51 +0800

    feat: improve app loading status

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,5 +1,20 @@
-import { Typography } from "@mui/material"
+import { Box, CircularProgress, Typography } from "@mui/material"
 
 export default function Loading() {
-  return <Typography>Loading...</Typography>
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        maxHeight: "400px",
+        width: "100%",
+      }}
+    >
+      <Typography>Loading...</Typography>
+      <CircularProgress />
+    </Box>
+  )
 }
