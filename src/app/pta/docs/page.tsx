f0483f2a6547e41330de479fb249a3fe96774f0a
Tony FUNG <twkfung@ust.hk>
2023-09-22 17:13:49 +0800

    feat: add pta docs fetching

--- src/app/pta/docs/page.tsx
+++ src/app/pta/docs/page.tsx
@@ -1,10 +1,13 @@
+import { Posts } from "@/lib/shared/components"
+import { WpEnv } from "@/lib/wpapi/WpEnv"
+
 export default function Page() {
   return (
     <main>
-      <section>會章</section>
-      <section>Under construction</section>
-      <section>財政報告</section>
-      <section>Under construction</section>
+      <Posts
+        categorySlug={WpEnv.CATEGORY_SLUGS.PTA_DOCS}
+        tagSlug={WpEnv.TAG_SLUGS.PTA_ALL_TIME}
+      />
     </main>
   )
 }
