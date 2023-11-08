4a43ea109c8c62125bb9cfd0d2dd05bae0b575ac
Tony FUNG <twkfung@ust.hk>
2023-11-08 18:26:22 +0800

    feat: add back navigation to post route

--- src/app/post/layout.tsx
+++ src/app/post/layout.tsx
@@ -1,8 +1,31 @@
+"use client"
+
 import { TitledLayout } from "@/lib/shared/components"
+import { Button, Stack } from "@mui/material"
+import { useRouter } from "next/navigation"
+import { ReactNode } from "react"
 
 const props = {
   title: "文章",
 }
 
 const PageLayout = TitledLayout(props)
-export default PageLayout
+const Layout = ({ children }: { children: ReactNode }) => {
+  const router = useRouter()
+  return (
+    <Stack sx={{ display: "flex" }}>
+      <PageLayout>{children}</PageLayout>
+      <Button
+        sx={{ flexGrow: 1 }}
+        variant="contained"
+        color="inherit"
+        onClick={() => {
+          router.back()
+        }}
+      >
+        返回
+      </Button>
+    </Stack>
+  )
+}
+export default Layout
