df0c672dbf99905f0196d071c4e9619c634e5275
Tony FUNG <twkfung@ust.hk>
2023-11-08 11:01:32 +0800

    feat: add 10-min staleTime to retrieve cached query

--- src/lib/react-query/index.tsx
+++ src/lib/react-query/index.tsx
@@ -6,6 +6,7 @@ export const queryClient = new QueryClient({
     queries: {
       retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000),
       retry: 3,
+      staleTime: 1000 * 60 * 10, // 10 minutes becomes stale
     },
   },
 })
